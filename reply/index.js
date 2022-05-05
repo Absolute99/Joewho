ab6a597398d0af8d408d53c82a69a12b47e7b9ab
Absolute99 <adamelfadil98@gmail.com>
2022-05-05 19:41:18 -0400

    !heheheeheheh

--- reply/index.js
+++ reply/index.js
@@ -25,47 +25,48 @@ message.channel.send('2/44 :postbox: ded sweg sous')
 message.channel.send('(carefully grabs your token)')
 message.delete()
 }
-if(message.content.toLowerCase().includes("!alexy")) {
-message.channel.send('')
-message.delete()
-}
+//if(message.content.toLowerCase().includes("!alexy")) {
+//message.channel.send('')
+//message.delete()
+//}
 if(message.content.toLowerCase().includes("!david")) {
-message.channel.send('')
+message.channel.send("I'M A SUSSY BAKA!")
+message.channel.send(":purple_heart: :smiling_imp: I LOVE LEAN!11111!!11!!!11! :purple_heart: :smiling_imp:")
 message.delete()
 }
-if(message.content.toLowerCase().includes("!zack")) {
-message.channel.send('')
-message.delete()
-}
-if(message.content.toLowerCase().includes("!bern")) {
-message.channel.send('')
-message.delete()
-}
+//if(message.content.toLowerCase().includes("!zack")) {
+//message.channel.send('')
+//message.delete()
+//}
+//if(message.content.toLowerCase().includes("!bern")) {
+//message.channel.send('')
+//message.delete()
+//}
 if(message.content.toLowerCase().includes("!oli")) {
 message.channel.send('ca marche pas... ca pas de jambes!')
 message.delete()
 }
-if(message.content.toLowerCase().includes("!alexis")) {
-message.channel.send('')
-message.delete()
-}
+//if(message.content.toLowerCase().includes("!alexis")) {
+//message.channel.send('')
+//message.delete()
+//}
 if(message.content.toLowerCase().includes("!morrin")) {
 message.channel.send('une vague the Purell inonde le serveur...')
-message.channel.send("vous vous sentez sous l'influence du Purell...")
+message.channel.send("vous vous sentez sous l'influence du Purell... :mask: :microbe: :syringe:")
 message.delete()
 }
 if(message.content.toLowerCase().includes("!noah")) {
-message.channel.send('')
+message.channel.send('GO TOUCH SOME GRASS!')
 message.delete()
 }
 if(message.content.toLowerCase().includes("!hubert")) {
-message.channel.send('')
+message.channel.send('ton étuis a disparu? heh heh heh...')
 message.delete()
 }
-if(message.content.toLowerCase().includes("!jacob")) {
-message.channel.send('')
-message.delete()
-}
+//if(message.content.toLowerCase().includes("!jacob")) {
+//message.channel.send('')
+//message.delete()
+//}
 if(message.content.toLowerCase().includes("!benoit")) {
 message.channel.send('WAY TO GO WAY TO GO 95% EN MATH FLEX FLEX FLEX JE SUIS LE MEILLEUR!!!!!11!!!111!1! :sunglasses:')
 message.delete()
