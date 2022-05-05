89430797b491d2741e5934f0161c3c2b12bda97d
Absolute99 <adamelfadil98@gmail.com>
2022-05-05 19:17:49 -0400

    !heheheha

--- reply/index.js
+++ reply/index.js
@@ -16,11 +16,66 @@ module.exports = {
            // message.channel.send('RIP BOZO')
        // }
         if(message.content.toLowerCase().includes("!adam")) {
-            message.channel.send(':flag_white: :ladder: 59')
+            message.channel.send(':flag_white: :ladder: 59%')
+            message.channel.send('13 - 4 = 1 ?!')
+            message.delete()
  }
- //if(message.content.toLowerCase().includes("!david")) {
-   // message.channel.send('24.212.53.167')
-//}
+ if(message.content.toLowerCase().includes("!sam")) {
+    message.channel.send('2/44 :postbox: ded sweg sous')
+    message.channel.send('(carefully grabs your token)')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!alexy")) {
+    message.channel.send('')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!david")) {
+    message.channel.send('')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!zack")) {
+    message.channel.send('')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!adamb")) {
+    message.channel.send('')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!oli")) {
+    message.channel.send('')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!alexis")) {
+    message.channel.send('')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!zackm")) {
+    message.channel.send('une vague the Purell inonde le serveur...')
+    message.channel.send(':mask: :microbe: :syringe:')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!noah")) {
+    message.channel.send('')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!hubert")) {
+    message.channel.send('')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!jacob")) {
+    message.channel.send('')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!benoit")) {
+    message.channel.send('WAY TO GO WAY TO GO 95% EN MATH FLEX FLEX FLEX JE SUIS LE MEILLEUR!!!!!11!!!111!1!')
+    message.delete()
+}
+if(message.content.toLowerCase().includes("!lucas")) {
+    message.channel.send('8% 1200$ dans valo')
+    message.channel.send('NICE 39%! ma note a QUADRUPLER! je suis trop fort.')
+    message.delete()
+}
+
         if(message.author.id == "") {
             message.delete()
            
